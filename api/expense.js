1c42ac37befe22c476ff29caa304af6752c450d7
Anuj Joshi <aj1097@usnh.edu>
2024-04-22 13:15:28 -0400

    Formatted files

--- api/expense.js
+++ api/expense.js
@@ -1,65 +1,71 @@
 import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { UpdateCommand, PutCommand, DynamoDBDocumentClient, ScanCommand, DeleteCommand } from "@aws-sdk/lib-dynamodb";
-import crypto from 'crypto';
+import {
+  UpdateCommand,
+  PutCommand,
+  DynamoDBDocumentClient,
+  ScanCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+import crypto from "crypto";
 
-const client = new DynamoDBClient({region: "us-east-1"});
+const client = new DynamoDBClient({ region: "us-east-1" });
 const docClient = DynamoDBDocumentClient.from(client);
 
 export const fetchExpenses = async () => {
-    const command = new ScanCommand({
-        ProjectionExpression: "id, amount, category, description",
-        TableName: "Expenses",
-    });
+  const command = new ScanCommand({
+    ProjectionExpression: "id, amount, category, description",
+    TableName: "Expenses",
+  });
 
-    const response = await docClient.send(command);
+  const response = await docClient.send(command);
 
-    return response;
+  return response;
 };
 
-export const createExpenses = async ({amount, category, description}) => {
-    const uuid = crypto.randomUUID()
-    const command = new PutCommand({
-        TableName: "Expenses",
-        Item: {
-            id: uuid,
-            amount,
-            category,
-            description
-        }
-    })
+export const createExpenses = async ({ amount, category, description }) => {
+  const uuid = crypto.randomUUID();
+  const command = new PutCommand({
+    TableName: "Expenses",
+    Item: {
+      id: uuid,
+      amount,
+      category,
+      description,
+    },
+  });
 
-    const response = await docClient.send(command);
+  const response = await docClient.send(command);
 
-    return response;
+  return response;
 };
 
-export const updateExpenses = async ( {id, amount, category, description}) => {
-    const command = new UpdateCommand({
-        TableName: "Expenses",
-        Key : {id},
-        UpdateExpression: "set amount = :a, category = :c, description: :d",
-        ExpressionAttributeValues: {
-            ":a": amount,
-            ":c": category,
-            ":d": description
-        },
-        ReturnValues: "ALL_NEW"
-    })
+export const updateExpenses = async ({ id, amount, category, description }) => {
+  const command = new UpdateCommand({
+    TableName: "Expenses",
+    Key: { id },
+    UpdateExpression: "set amount = :a, category = :c, description: :d",
+    ExpressionAttributeValues: {
+      ":a": amount,
+      ":c": category,
+      ":d": description,
+    },
+    ReturnValues: "ALL_NEW",
+  });
 
-    const response = await docClient.send(command);
+  const response = await docClient.send(command);
 
-    return response;
+  return response;
 };
 
 export const deleteExpenses = async (id) => {
-    const command = new DeleteCommand({
-        TableName: "Expenses",
-        Key: {
-            id
-        }
-    });
+  const command = new DeleteCommand({
+    TableName: "Expenses",
+    Key: {
+      id,
+    },
+  });
 
-    const response = await docClient.send(command);
+  const response = await docClient.send(command);
 
-    return response;
+  return response;
 };
--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createExpenses, deleteExpenses, fetchExpenses, updateExpenses } from "./expense.js";
+import {
+  createExpenses,
+  deleteExpenses,
+  fetchExpenses,
+  updateExpenses,
+} from "./expense.js";
 import serverless from "serverless-http";
-import cors from 'cors';
-
+import cors from "cors";
 
 const app = express();
 const port = 3001;
@@ -13,7 +17,6 @@ if (process.env.DEVELOPMENT) {
   app.use(cors());
 }
 
-
 app.get("/", (req, res) => {
   res.send("Hello from express!");
 });
@@ -21,45 +24,45 @@ app.get("/", (req, res) => {
 app.get("/expense", async (req, res) => {
   try {
     const expenses = await fetchExpenses();
-    res.send(expenses.Items)
+    res.send(expenses.Items);
   } catch (e) {
-    res.status(400).send(`Error Fetching expenses: ${e}`)
+    res.status(400).send(`Error Fetching expenses: ${e}`);
   }
 });
 
 app.post("/expense", async (req, res) => {
   try {
     const expense = req.body;
-    
+
     const response = await createExpenses(expense);
 
     res.send(response);
   } catch (e) {
-    res.status(400).send(`Error Creating expenses: ${e}`)
+    res.status(400).send(`Error Creating expenses: ${e}`);
   }
 });
 
 app.put("/expense", async (req, res) => {
   try {
     const expense = req.body;
-    
+
     const response = await updateExpenses(expense);
 
     res.send(response);
   } catch (e) {
-    res.status(400).send(`Error Updating expenses: ${e}`)
+    res.status(400).send(`Error Updating expenses: ${e}`);
   }
 });
 
 app.delete("/expense/:id", async (req, res) => {
   try {
-    const {id} = req.params;
-    
+    const { id } = req.params;
+
     const response = await deleteExpenses(id);
 
     res.send(response);
   } catch (e) {
-    res.status(400).send(`Error Deleting expenses: ${e}`)
+    res.status(400).send(`Error Deleting expenses: ${e}`);
   }
 });
 
@@ -68,6 +71,5 @@ if (process.env.DEVELOPMENT) {
     console.log(`Example app listening at http://localhost:${port}`);
   });
 }
-
 
 export const handler = serverless(app);
